4e9d80fbdc5309d433f36b3e700270cbf2f9289c
baharma <bagusaditya1899@gmail.com>
2023-05-25 02:38:29 +0800

    ad

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +27,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+          if (env("APP_ENV") !== "local") {
+            url::forceScheme("https");
+        }
     }
 }
